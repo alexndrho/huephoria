2d37d67888dbb82177d7f5021b841afeea0e6560
Alexander Ho <ho.alexander.g@gmail.com>
2024-04-15 14:41:38 +0800

    feat: scroll to top on route change

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './hooks/ScrollToTop.tsx';
 import App from './App.tsx';
 import theme from './theme.ts';
 import '@mantine/core/styles.css';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider defaultColorScheme="dark" theme={theme}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </MantineProvider>
